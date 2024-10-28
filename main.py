594e3d255d4d09083959dbc94470b6ba05adba44
JeasunLok <1913587141@qq.com>
2024-10-28 11:30:58 +0800

    fix: set IsResize = True

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=epoch//2, gamma=0.9) 
     criterion = nn.CrossEntropyLoss().cuda()
 
-    image_transform = get_transform(input_shape, IsTotensor=True, IsRandomRotation=True) # set IsResize=True to make sure it works
+    image_transform = get_transform(input_shape, IsResize=True, IsTotensor=True, IsRandomRotation=True) # set IsResize=True to make sure it works
     train_dataset = MyDataset(train_lines, input_shape=input_shape, transform=image_transform)
     val_dataset = MyDataset(val_lines, input_shape=input_shape, transform=image_transform)
     test_dataset = MyDataset(test_lines, input_shape=input_shape, transform=image_transform)
